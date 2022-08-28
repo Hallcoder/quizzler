418b0e8ec56972c4f3056ea96fc3ee0866dbcfd3
Hallcoder <hallcoder25@gmail.com>
2022-08-28 13:11:53 +0200

    Pillars of OOP:abstraction,encapsulation,inheritance and Polymorphism

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 //TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(const Quizzler());
 
@@ -30,31 +34,24 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = const [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-        Icons.close,
-        color:Colors.red
-    )
-  ];
+  List<Icon> scoreKeeper =  [];
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        const Expanded(
+         Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(questionNumber),
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -77,7 +74,27 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                ;
+                bool correctAnswer = quizBrain.questionsBank[questionNumber].qAnswer;
+                if(correctAnswer == true){
+                  print('User got it right');
+                  scoreKeeper.add(const Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                }else{
+                  print('User got it wrong');
+                  scoreKeeper.add(const Icon(
+                      Icons.close,
+                      color:Colors.red
+                  ));
+                };
+                int q = questionNumber;
+                q++;
+                if(q< quizBrain.questionsBank.length){
+                  setState(() {
+                    questionNumber++;
+                  });
+                }
               },
             ),
           ),
@@ -96,6 +113,27 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.;
+                bool correctAnswer = quizBrain.questionsBank[questionNumber].qAnswer;
+                if(correctAnswer == false){
+                  scoreKeeper.add(const Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                  print('User got it right');
+                }else{
+                  print('User got it wrong');
+                  scoreKeeper.add(const Icon(
+                      Icons.close,
+                      color:Colors.red
+                  ));
+                }
+                int q = questionNumber;
+                q++;
+                if(q < quizBrain.questionsBank.length){
+                  setState(() {
+                    ++questionNumber;
+                  });
+                }
               },
             ),
           ),
@@ -111,7 +149,7 @@ class _QuizPageState extends State<QuizPage> {
 
 
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
+ 'You can lead a cow down stairs but not up stairs.', false,
+'Approximately one quarter of human bones are in the feet.', true,
+'A slug\'s blood is green.', true,
 */
---
+++ lib/question.dart
@@ -0,0 +1,10 @@
+class Question{
+  late String questionText;
+  late  bool qAnswer;
+
+   Question(String s, bool answer){
+    qAnswer = answer;
+    questionText = s;
+  }
+
+}
